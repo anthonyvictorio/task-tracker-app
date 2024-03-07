bdd971d2dbe078cc2cf83f7fa35983516753914d
Anthony Victorio <anthonyvictorio@Anthonys-MacBook-Pro.local>
2024-03-07 15:20:24 -0500

    Implement Javascript functionallity

--- script.js
+++ script.js
@@ -22,21 +22,29 @@ function handleSubmission(event) {
     event.preventDefault();
     // TODO: Get form input values
     const taskName = document.getElementById("taskName").value
-    const taskDesc = document.getElementById("taskDescription").value
+    const taskDescription = document.getElementById("taskDescription").value
     const taskDeadline = document.getElementById("taskDeadline").value
     // TODO: Validate input fields
     if (String(taskName) === "" || String(taskDeadline) === ""){
         alert('Task name and deadline are required')
     }
     // TODO: Update the tasks array
-    tasks.push(taskName, taskDesc, taskDeadline)
+    tasks.push({name: taskName, description: taskDescription, deadline: taskDeadline})
     render();
     }
 
     // Function to render tasks in the table
     function render() {
      // TODO: Use array methods to create a new table row of data for each item in the arr
-     taskTable.innerHTML = tasks.map
+     taskTable.innerHTML = tasks.map( task =>`
+        <tr>
+            <td>${task.name}</td>
+            <td>${task.description}</td>
+            <td>${task.deadline}</td>
+            <td><button onclick="markTaskComlete(this)">Complete</button></td>
+            <td><button onclick="removeTask(this)">Remove</button></td>
+        </tr>
+        `).join('')
     }
     // Function to initialize the table
     function init() {
@@ -44,3 +52,9 @@ function handleSubmission(event) {
         tasks = []; // Reset the tasks array
         render(); // Call the render function
     }
+
+    // Event ;istener for form submission
+    taskForm.addEventListener('submit',handleSubmission)
+
+    // Call init function to set up initial state of the app
+    init();
