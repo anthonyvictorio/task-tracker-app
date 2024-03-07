c8390fc2b9692a885fc2a25a4c70e82bbe24f366
Anthony Victorio <anthonyvictorio@Anthonys-MacBook-Pro.local>
2024-03-07 14:39:14 -0500

    Implement form submission handling

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ let tasks = [];
 // Section 3: Cached Element References
 const taskForm = document.getElementById("taskForm")
 const taskTable = document.getElementById("taskTable") 
+
+// Function to handle form submissions
+function handleSubmission(event) {
+    event.preventDefault();
+    // TODO: Get form input values
+    const taskName = document.getElementById("taskName").value
+    const taskDesc = document.getElementById("taskDescription").value
+    const taskDeadline = document.getElementById("taskDeadline").value
+    // TODO: Validate input fields
+    if (taskName === "" || taskDeadline === ""){
+        alert('Task name and deadline are required')
+    }
+    // TODO: Update the tasks array
+    tasks.push(taskName, taskDesc, taskDeadline)
+    render();
+    }
+
+    // Function to render tasks in the table
+    function render() {
+        // TODO: Use array methods to create a new table row of data for each item in the arr
+    }
+    // Function to initialize the table
+    function init() {
+        taskTable.innerHTML = ''; // Clear the table
+        tasks = []; // Reset the tasks array
+        render(); // Call the render function
+    }
